1031242ad04831e53616a5edc5fb62c0100b68d7
Dylan Harper <dylanharper4@icloud.com>
2022-01-19 00:22:21 -0800

    Add test data and look for edge cases in discount revenue methods

--- spec/models/invoice_spec.rb
+++ spec/models/invoice_spec.rb
@@ -24,37 +24,45 @@ RSpec.describe Invoice, type: :model do
       expect(@invoice_1.total_revenue).to eq(100)
     end
 
-    it "discount_amount" do
+    before :each do
       @merchant1 = Merchant.create!(name: 'Hair Care')
+      @merchant2 = Merchant.create!(name: 'Bare Bear')
+
       @item_1 = Item.create!(name: "Shampoo", description: "This washes your hair", unit_price: 10, merchant_id: @merchant1.id, status: 1)
       @item_8 = Item.create!(name: "Butterfly Clip", description: "This holds up your hair but in a clip", unit_price: 5, merchant_id: @merchant1.id)
+
       @customer_1 = Customer.create!(first_name: 'Joey', last_name: 'Smith')
+      @customer_2 = Customer.create!(first_name: 'Jeff', last_name: 'Jones')
+
       @invoice_1 = Invoice.create!(customer_id: @customer_1.id, status: 2, created_at: "2012-03-27 14:54:09")
+      @invoice_2 = Invoice.create!(customer_id: @customer_1.id, status: 2, created_at: "2012-03-27 14:54:09")
+      @invoice_3 = Invoice.create!(customer_id: @customer_2.id, status: 2, created_at: "2012-03-27 14:54:09")
+      @invoice_4 = Invoice.create!(customer_id: @customer_2.id, status: 2, created_at: "2012-03-27 14:54:09")
+
       @ii_1 = InvoiceItem.create!(invoice_id: @invoice_1.id, item_id: @item_1.id, quantity: 9, unit_price: 10, status: 2)
-      @ii_11 = InvoiceItem.create!(invoice_id: @invoice_1.id, item_id: @item_8.id, quantity: 4, unit_price: 10, status: 1)
+      @ii_2 = InvoiceItem.create!(invoice_id: @invoice_1.id, item_id: @item_8.id, quantity: 4, unit_price: 10, status: 1)
+      @ii_3 = InvoiceItem.create!(invoice_id: @invoice_2.id, item_id: @item_8.id, quantity: 4, unit_price: 10, status: 1)
+      @ii_4 = InvoiceItem.create!(invoice_id: @invoice_2.id, item_id: @item_8.id, quantity: 4, unit_price: 10, status: 1)
+      @ii_5 = InvoiceItem.create!(invoice_id: @invoice_1.id, item_id: @item_8.id, quantity: 2, unit_price: 10, status: 1)
+      @ii_6 = InvoiceItem.create!(invoice_id: @invoice_3.id, item_id: @item_8.id, quantity: 2, unit_price: 10, status: 1)
+      @ii_7 = InvoiceItem.create!(invoice_id: @invoice_4.id, item_id: @item_8.id, quantity: 2, unit_price: 10, status: 1)
 
       @discount_1 = @merchant1.bulk_discounts.create!(percent_discount: 0.20, quantity_threshold: 4)
       @discount_2 = @merchant1.bulk_discounts.create!(percent_discount: 0.30, quantity_threshold: 8)
       @discount_3 = @merchant1.bulk_discounts.create!(percent_discount: 0.35, quantity_threshold: 20)
       @discount_4 = @merchant1.bulk_discounts.create!(percent_discount: 0.40, quantity_threshold: 25)
 
-      expect(@invoice_1.discounted).to eq([@ii_1, @ii_11])
+      @discount_5 = @merchant2.bulk_discounts.create!(percent_discount: 0.20, quantity_threshold: 1)
+      @discount_6 = @merchant2.bulk_discounts.create!(percent_discount: 0.30, quantity_threshold: 1)
+      @discount_7 = @merchant2.bulk_discounts.create!(percent_discount: 0.35, quantity_threshold: 1)
+      @discount_8 = @merchant2.bulk_discounts.create!(percent_discount: 0.40, quantity_threshold: 1)
+    end
+
+    it "discount_amount" do
+      expect(@invoice_1.discounted).to eq([@ii_1, @ii_2])
     end
 
     it "bd_total_revenue" do
-      @merchant1 = Merchant.create!(name: 'Hair Care')
-      @item_1 = Item.create!(name: "Shampoo", description: "This washes your hair", unit_price: 10, merchant_id: @merchant1.id, status: 1)
-      @item_8 = Item.create!(name: "Butterfly Clip", description: "This holds up your hair but in a clip", unit_price: 5, merchant_id: @merchant1.id)
-      @customer_1 = Customer.create!(first_name: 'Joey', last_name: 'Smith')
-      @invoice_1 = Invoice.create!(customer_id: @customer_1.id, status: 2, created_at: "2012-03-27 14:54:09")
-      @ii_1 = InvoiceItem.create!(invoice_id: @invoice_1.id, item_id: @item_1.id, quantity: 9, unit_price: 10, status: 2)
-      @ii_11 = InvoiceItem.create!(invoice_id: @invoice_1.id, item_id: @item_8.id, quantity: 4, unit_price: 10, status: 1)
-
-      @discount_1 = @merchant1.bulk_discounts.create!(percent_discount: 0.20, quantity_threshold: 4)
-      @discount_2 = @merchant1.bulk_discounts.create!(percent_discount: 0.30, quantity_threshold: 8)
-      @discount_3 = @merchant1.bulk_discounts.create!(percent_discount: 0.35, quantity_threshold: 20)
-      @discount_4 = @merchant1.bulk_discounts.create!(percent_discount: 0.40, quantity_threshold: 25)
-
       expect(@invoice_1.bd_total_revenue).to eq(95)
     end
   end
